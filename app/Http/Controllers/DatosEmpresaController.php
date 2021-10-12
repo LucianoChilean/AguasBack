fac46afc1b1e0500d6dd68a72c8e62deaedae1eb
Luciano Saavedra Mattensohn <mattensohn64@gmail.com>
2021-10-11 23:05:02 -0300

    Funciones básicas
    Se añade CRUD a todos los controladores

--- app/Http/Controllers/DatosEmpresaController.php
+++ app/Http/Controllers/DatosEmpresaController.php
@@ -2,9 +2,170 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\datos_empresa;
+use Illuminate\Http\{Request, Response};
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class DatosEmpresaController extends Controller
 {
-    //
+    public function index(){
+        return datos_empresa::all();
+    }
+
+    public function detail($id){
+
+        $perfil = datos_empresa::find($id);
+
+        if(is_object($perfil)){
+
+            $data = array(
+                'code'   => 200,
+                'status' => 'success',
+                'perfil'   => $perfil
+            );
+
+        }else{
+            $data = array(
+                'code'      => 404,
+                'status'    => 'error',
+                'message'   => 'usuario no existe'
+            );
+        }
+
+        return response()->json($data, $data['code']);
+
+    }
+    public function create(Request $request){
+    
+        $json = $request->input('json',null);
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true);//array
+       
+        $params_array = array_map('trim', $params_array);
+
+      
+        if(!empty($params) && !empty($params_array)){
+
+        $validate = \validator($params_array,[
+            'nombre'                  => 'required|alpha',
+            'descripcion'             => 'regex:/^[\pL\s\-]+$/u'
+        ]);
+
+    
+        if($validate->fails()){
+            $data = array(
+                'status'   => 'error',
+                'code'     => 404,
+                'message'  => 'Datos incorrectos',
+                'errors'   => $validate->errors()
+            );
+        }else{
+          
+            //Crear Perfiles
+            $permisos = new datos_empresa();
+            $permisos->nombre                = $params_array['nombre'];
+            $permisos->descripcion           = $params_array['descripcion'];
+
+            $permisos->save();
+
+            $data = array(
+                'status'   => 'success',
+                'code'     => 200,
+                'message'  => 'Permiso Creado Correctamente'
+            );
+
+        }
+
+    }else{
+        $data = array(
+            'status'   => 'error',
+            'code'     => 404,
+            'message'  => 'Datos enviados de forma incorrecta'
+        );
+
+    }
+
+        return response()->json($data, $data['code']);
+       
+    }
+
+    public function update(Request $request){
+
+        $json = $request->input('json',null);
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true);//array
+       
+        $params_array = array_map('trim', $params_array);
+
+        if(!empty($params) && !empty($params_array)){
+
+            $validate = \validator($params_array,[
+                'nombre'                  => 'required|alpha',
+                'descripcion'             => 'regex:/^[\pL\s\-]+$/u'
+            ]);
+    
+        
+            if($validate->fails()){
+                $data = array(
+                    'status'   => 'error',
+                    'code'     => 404,
+                    'message'  => 'Datos incorrectos',
+                    'errors'   => $validate->errors()
+                );
+
+            }else{
+              
+                //Actualiza Perfil
+                datos_empresa::where('id',$params_array['id'])->update($params_array);
+  
+                $data = array(
+                    'code'    => 200,
+                    'status'  => 'success',
+                    'changes' => $params_array
+                );
+    
+            
+    
+            }
+    
+        }else{
+
+            $data = array(
+                'status'   => 'error',
+                'code'     => 404,
+                'message'  => 'Datos enviados de forma incorrecta'
+            );
+    
+        }
+    
+            return response()->json($data, $data['code']);
+      
+    }
+
+
+    public function delete(Request $request, $id){
+        $user = datos_empresa::find($id);
+
+        if(is_object($user)){
+
+            $data = array(
+                'code'      => 200,
+                'status'    => 'success',
+                'message'   => 'Permiso a sido eliminado'
+            );
+
+            $user->delete();
+
+        }else{
+            $data = array(
+                'code'      => 404,
+                'status'    => 'error',
+                'message'   => 'Permiso no existe'
+            );
+        }
+
+        return response()->json($data, $data['code']);
+        
+    }
 }
--- app/Http/Controllers/HojaRutaController.php
+++ app/Http/Controllers/HojaRutaController.php
@@ -2,9 +2,170 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\hoja_ruta;
+use Illuminate\Http\{Request, Response};
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class HojaRutaController extends Controller
 {
-    //
+    public function index(){
+        return hoja_ruta::all();
+    }
+
+    public function detail($id){
+
+        $perfil = hoja_ruta::find($id);
+
+        if(is_object($perfil)){
+
+            $data = array(
+                'code'   => 200,
+                'status' => 'success',
+                'perfil'   => $perfil
+            );
+
+        }else{
+            $data = array(
+                'code'      => 404,
+                'status'    => 'error',
+                'message'   => 'usuario no existe'
+            );
+        }
+
+        return response()->json($data, $data['code']);
+
+    }
+    public function create(Request $request){
+    
+        $json = $request->input('json',null);
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true);//array
+       
+        $params_array = array_map('trim', $params_array);
+
+      
+        if(!empty($params) && !empty($params_array)){
+
+        $validate = \validator($params_array,[
+            'nombre'                  => 'required|alpha',
+            'descripcion'             => 'regex:/^[\pL\s\-]+$/u'
+        ]);
+
+    
+        if($validate->fails()){
+            $data = array(
+                'status'   => 'error',
+                'code'     => 404,
+                'message'  => 'Datos incorrectos',
+                'errors'   => $validate->errors()
+            );
+        }else{
+          
+            //Crear Perfiles
+            $permisos = new hoja_ruta();
+            $permisos->nombre                = $params_array['nombre'];
+            $permisos->descripcion           = $params_array['descripcion'];
+
+            $permisos->save();
+
+            $data = array(
+                'status'   => 'success',
+                'code'     => 200,
+                'message'  => 'Permiso Creado Correctamente'
+            );
+
+        }
+
+    }else{
+        $data = array(
+            'status'   => 'error',
+            'code'     => 404,
+            'message'  => 'Datos enviados de forma incorrecta'
+        );
+
+    }
+
+        return response()->json($data, $data['code']);
+       
+    }
+
+    public function update(Request $request){
+
+        $json = $request->input('json',null);
+        $params = json_decode($json); //objeto
+        $params_array = json_decode($json, true);//array
+       
+        $params_array = array_map('trim', $params_array);
+
+        if(!empty($params) && !empty($params_array)){
+
+            $validate = \validator($params_array,[
+                'nombre'                  => 'required|alpha',
+                'descripcion'             => 'regex:/^[\pL\s\-]+$/u'
+            ]);
+    
+        
+            if($validate->fails()){
+                $data = array(
+                    'status'   => 'error',
+                    'code'     => 404,
+                    'message'  => 'Datos incorrectos',
+                    'errors'   => $validate->errors()
+                );
+
+            }else{
+              
+                //Actualiza Perfil
+                hoja_ruta::where('id',$params_array['id'])->update($params_array);
+  
+                $data = array(
+                    'code'    => 200,
+                    'status'  => 'success',
+                    'changes' => $params_array
+                );
+    
+            
+    
+            }
+    
+        }else{
+
+            $data = array(
+                'status'   => 'error',
+                'code'     => 404,
+                'message'  => 'Datos enviados de forma incorrecta'
+            );
+    
+        }
+    
+            return response()->json($data, $data['code']);
+      
+    }
+
+
+    public function delete(Request $request, $id){
+        $user = hoja_ruta::find($id);
+
+        if(is_object($user)){
+
+            $data = array(
+                'code'      => 200,
+                'status'    => 'success',
+                'message'   => 'Permiso a sido eliminado'
+            );
+
+            $user->delete();
+
+        }else{
+            $data = array(
+                'code'      => 404,
+                'status'    => 'error',
+                'message'   => 'Permiso no existe'
+            );
+        }
+
+        return response()->json($data, $data['code']);
+        
+    }
 }
